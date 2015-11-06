cc839935b8c33143df1726101a1e38ba8a5d8594
John Wyles <john@fourstacks.co.uk>
2015-11-06 21:50:05 +0000

    add card table migration and remove default migrations

--- database/migrations/2015_11_06_195933_add_cards_table.php
+++ database/migrations/2015_11_06_195933_add_cards_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddCardsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cards', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('type');
+            $table->string('rarity');
+            $table->integer('cost');
+            $table->integer('attack');
+            $table->integer('health');
+            $table->string('text');
+            $table->string('flavor');
         });
     }
 
@@ -29,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('cards');
     }
 }
